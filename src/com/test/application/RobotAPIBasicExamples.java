d70b1a1e4889dfff9342d631171f3b95845fb6a3
RobinsonNguyen <robinson_nguyen@hotmail.com>
2025-07-10 16:54:38 -0400

    --- automatic rebase to remote; old local SHA: a807e8dd5cce2729e92e03046b670065d1bf41c6 ---

---
+++ src/com/test/application/RobotAPIBasicExamples.java
@@ -0,0 +1,53 @@
+package com.test.application;
+
+
+import javax.inject.Inject;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import com.kuka.roboticsAPI.geometricModel.ObjectFrame;
+import com.kuka.roboticsAPI.geometricModel.World;
+
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.med.deviceModel.LBRMed;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class RobotAPIBasicExamples extends RoboticsAPIApplication {
+	@Inject
+	private LBRMed robot;
+	//@Inject 
+	//private ISceneGraph sceneGraph;
+	@Inject 
+	private World world;
+
+	@Override
+	public void initialize() {
+		// initialize your application here
+	}
+
+	@Override
+	public void run() {
+		// your application execution starts here
+		getLogger().info("Hello World");
+		
+		robot.getFlange().move(ptpHome());
+		//sceneGraph.getWorld().findFrame("/P1");
+		ObjectFrame targetFrame = world.getFrame("P1");
+		robot.getFlange().move(lin(targetFrame));
+	}
+}
